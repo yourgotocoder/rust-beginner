3396ddcabb55c9d6204ba1e1196291cad33f3174
yourgotocoder <yourgotocoder@gmail.com>
2022-05-30 10:34:07 +0530

    Using functions and  if conditions

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,26 @@ use std::env::{args, Args};
 
 fn main() {
     let mut args: Args = args();
-    let first = args.nth(0);
-    println!("{:?}", first);
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number: f32 = first.parse().unwrap();
+    let second_number: f32 = second.parse().unwrap();
+    let result = operate(operator, first_number, second_number);
+    println!("{:?}", result);
+}
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+    if operator == '+' {
+        first_number + second_number
+    } else if operator == '-' {
+        first_number - second_number
+    } else if operator == '/' {
+        first_number / second_number
+    } else if operator == '*' {
+        first_number * second_number
+    } else {
+        0.0
+    }
 }
