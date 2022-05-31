f744c7cc5d36b0fce30c0666890c88ff17ff4a8b
yourgotocoder <yourgotocoder@gmail.com>
2022-05-31 12:07:16 +0530

    Returning values from loops

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
-use rand::Rng;
-use std::{cmp::Ordering, io};
-
 fn main() {
-    println!("Guess the number!!");
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut counter = 0;
 
-    loop {
-        println!("Please input the guess.");
+    let result = loop {
+        counter += 1;
 
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        println!("You guessed {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            }
+        if counter == 10 {
+            break counter * 2;
         }
-    }
+    };
+
+    println!("The result is {}", result);
 }
