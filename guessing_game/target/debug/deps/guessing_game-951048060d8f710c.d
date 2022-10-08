5d411eb4357659b851bb7714f535e7e3bd01b001
yourgotocoder <yourgotocoder@gmail.com>
2022-10-08 10:31:21 +0530

    Taking input and showing output basics

---
+++ guessing_game/target/debug/deps/guessing_game-951048060d8f710c.d
@@ -0,0 +1,5 @@
+/home/cse/Projects/Courses/rust-beginner/guessing_game/target/debug/deps/guessing_game-951048060d8f710c: src/main.rs
+
+/home/cse/Projects/Courses/rust-beginner/guessing_game/target/debug/deps/guessing_game-951048060d8f710c.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/cse/Projects/Courses/rust-beginner/guessing_game/target/debug/guessing_game: /home/cse/Projects/Courses/rust-beginner/guessing_game/src/main.rs
