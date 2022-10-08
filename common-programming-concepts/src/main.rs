5b3b16e168325e5b4fcf5365be80db9ea1cc4d68
yourgotocoder <yourgotocoder@gmail.com>
2022-10-08 13:20:59 +0530

    Named loops

--- common-programming-concepts/src/main.rs
+++ common-programming-concepts/src/main.rs
@@ -1,12 +1,21 @@
 fn main() {
-    let mut counter = 0;
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count: {count}");
+        let mut remaining = 10;
 
-    let result = loop {
-        counter += 1;
-
-        if counter == 10 {
-            break counter * 2;
+        loop {
+            println!("remianing: {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
         }
-    };
-    println!("The result is {result}");
+        count += 1;
+    }
+    println!("End count is {count}");
+
 }
