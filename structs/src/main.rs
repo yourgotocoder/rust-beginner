8134481f7ef367cfb67caca2ad364ac6554684d5
yourgotocoder <yourgotocoder@gmail.com>
2022-10-28 16:23:21 +0530

    Area calc

---
+++ structs/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let width = 30;
+    let height = 50;
+    println!(
+        "The area of the reactangle is {} square pixels",
+        area(width, height)
+    );
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
