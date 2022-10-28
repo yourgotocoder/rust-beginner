bba97e4c9f08a63f17a4706b8eca243df55e55e3
yourgotocoder <yourgotocoder@gmail.com>
2022-10-28 15:17:08 +0530

    Using slices

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     println!("The first space of '{}' is at {}.", s1, len);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
